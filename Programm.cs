1683dc7e096781f3af0cf489a1f3b72adea39b7c
SlykaparStuff <125670632+SlykaparStuff@users.noreply.github.com>
2023-02-17 15:10:15 +0100

    Create Programm.cs

---
+++ Programm.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeVbucks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            System.Diagnostics.Process.Start("https://youtu.be/QB7ACr7pUuE&autoplay=1");
+
+        }
+    }
+}
